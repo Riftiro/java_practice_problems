ba8c0efa106c1b3912941a93946169d431e4ba45
TiniWolf <tiniwolf76@gmail.com>
2019-12-21 15:49:08 -0500

    someBODY

--- src/rock_paper_scissors/RockyV1.java
+++ src/rock_paper_scissors/RockyV1.java
@@ -12,18 +12,17 @@ public class RockyV1 {
         print("Welcome to rock, paper, scissors!\nPlease select a weapon\n1 = Rock\n2 = Paper\n3 = Scissors");
         int humanSelection = scanner.nextInt() - 1;
         int botSelection = rand.nextInt(3);    
-        String outcomes[] = {"You both chose " + weapons[botSelection] + "! You canceled out each other's hits!", "You killed your opponent!", "Your opponent killed you!"};
+        String outcomes[] = {"You both chose " + weapons[botSelection] + "! You canceled out each other's hits!", "You killed your opponent with " + weapons[humanSelection] + "!", "Your opponent killed you with" + weapons[botSelection] + "!"};
 
-        if(humanSelection==0) {
-            if(botSelection==0){
-                System.out.println(outcomes[0]);
-            }else if(botSelection==1){
-                System.out.println(outcomes[1]);
-                
-            }
+        if(humanSelection==botSelection) {
+            System.out.println(outcomes[0]);  
+        }else if(humanSelection==1 && botSelection==0 || humanSelection == 2 && botSelection == 1 || humanSelection == 3 && botSelection == 2){
+            System.out.println(outcomes[1]);
+        }else if(humanSelection==1 && botSelection ==2 || humanSelection ==2 && botSelection==3 || humanSelection == 3 && botSelection==0){
+            System.out.println(outcomes[2]);
         }
     }
-    
+
     public static void print(String text) {
         System.out.println(text);
     }
